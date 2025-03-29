a7dfe611f59ef9d0b9edd9ce2ec7ad11f7692be7
PANdeVInCent <yeuemnhieu3377@gmail.com>
2025-03-29 21:34:15 +0700

    update login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,8 +19,37 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 const auth = getAuth(app);
 
+// //tính năng đăng kí
+// document.addEventListener('DOMContentLoaded', function() {
+//     const signUpButton = document.getElementById('signup');
+
+//     signUpButton.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         const email = document.getElementById('rEmail').value;
+//         const password = document.getElementById('rPassword').value;
+//         const cpassword = document.getElementById('cPassword').value;
+
+//         createUserWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             //Đã đăng kí
+//             const user = userCredential.user;
+//             set(ref(database, 'users/' + user.uid), {
+//                 email: email,
+//                 password: password,
+//                 cpassword: cpassword,
+//             })
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+
+//             alert(errorMessage, 'signUpMessage');
+//         });
+//     });
+//     });
 //submit button
 const submit = document.getElementById('submit');
 const loginBox = document.querySelector("main");
@@ -33,8 +63,11 @@ submit.addEventListener('click', function (event) {
     .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
         window.location.href = "../pages/grand.html";
-        // ...
+        update(ref(database, 'users/'+ user.uid), {
+            last_login: dt,
+        });
     })
     .catch((error) => {
         const errorMessage = error.message;
